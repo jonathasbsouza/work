4b156cb7259913fa0ff9edcf2ba01dfbaa8adda1
Jonathas Souza <jonathassouza@gmail.com>
2022-02-11 21:31:52 -0300

    add basic My Work structure

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from "../components/layout";
 import HeroGrid from "../components/heroGrid";
 
 import theme from "../styles/theme";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Stack, Heading, Text } from "@chakra-ui/react";
 import BaseContainer from "../components/BaseContainer";
 
 export default function Home() {
@@ -16,14 +16,51 @@ export default function Home() {
         <HeroGrid />
       </BaseContainer>
       <Box
-        sx={{
-          backgroundColor: theme.colors.fauxblack,
-        }}
+        id="whoami"
+        bg={theme.colors.lightBeige}
+        pt="18rem"
+        pb="4rem"
+        mt={-232}
       >
         <BaseContainer>
           <p>
             (This is a sample website - you’ll be building a site like this on{" "}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </BaseContainer>
+      </Box>
+      <Box id="work" py="10rem">
+        <BaseContainer>
+          <Stack direction={["column", "row"]} spacing="3rem">
+            <Stack
+              direction="column"
+              spacing="2rem"
+              align="flex-start"
+              flex="1"
+            >
+              <Heading as="h4" size="md">
+                My Work
+              </Heading>
+              <Text fontSize="sm">
+                In 10 years, I've worked in a lot of design disciplines, as
+                Graphic, Editorial and Product Design. Here is some examples of
+                this experience.
+              </Text>
+              <Button colorScheme="blue">Let's talk</Button>
+            </Stack>
+            <Box flex="4" bg="tomato">
+              <Text>Box 3</Text>
+            </Box>
+          </Stack>
+        </BaseContainer>
+      </Box>
+      <Box bg={theme.colors.fauxblack} color="white" py="1.5rem">
+        <BaseContainer>
+          <p>
+            reach me at{" "}
+            <a href="mailto:jonathasbsouza@gmail.com">
+              jonathasbsouza@gmail.com
+            </a>
           </p>
         </BaseContainer>
       </Box>
--- styles/theme.js
+++ styles/theme.js
@@ -3,6 +3,8 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     fauxblack: "#454346",
+    lightBeige: "#F8F4EF",
+    white: "#fff",
   },
   fonts: {
     heading: "Fraunces",
